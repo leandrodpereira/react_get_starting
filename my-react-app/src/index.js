d3954a99e9eaade4f010056d2bdcd2e4cebcaaa2
Leandro <leandro.pereira@fepi.br>
2024-01-08 16:59:51 -0300

    JSX

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello Word!</h1>
+//const myFirstElement = <h1>Hello Word!</h1>
+
+const myelement = (
+    <div>
+        <h1>Hello Word!</h1>
+       
+        <table>
+        <tr>
+            <th>Name</th>       
+        </tr>
+        <tr>
+            <td>John</td>
+        </tr>
+        <tr>
+            <td>Elsa</td>
+        </tr>      
+        </table>
+    </div>
+  );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(myelement);
